b447e127ecbad61ccaac34c948d3516387d26c62
kimdaebeom <68265609+kimdaebeom@users.noreply.github.com>
2021-07-05 17:46:47 +0900

    Update Salary_linear_regression.py

--- linear_regression/Salary_linear_regression.py
+++ linear_regression/Salary_linear_regression.py
@@ -1,38 +1,52 @@
-# import os os.environ['KAGGLE_USERNAME'] = 'username' # username os.environ['KAGGLE_KEY'] = 'key' # key
+# import os os.environ['KAGGLE_USERNAME'] = 'username' # username 
+# os.environ['KAGGLE_KEY'] = 'key' # key
 # !kaggle datasets download -d rsadiq/salary
 # !unzip salary.zip
 
-from tensorflow.keras.models import Sequential 
-from tensorflow.keras.layers import Dense 
-from tensorflow.keras.optimizers import Adam, SGD 
-import numpy as np 
-import pandas as pd 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam, SGD
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt 
-import seaborn as sns 
-from sklearn.model_selection import train_test_split 
+import seaborn as sns
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('Salary.csv')
 
-x_data = np.array(df['YearsExperience'], dtype=np.float32) 
-y_data = np.array(df['Salary'], dtype=np.float32) 
 
-x_data = x_data.reshape((-1, 1)) 
-y_data = y_data.reshape((-1, 1)) 
 
-print(x_data.shape) 
-print(y_data.shape) 
+x_data = np.array(df['YearsExperience'], dtype=np.float32)
+y_data = np.array(df['Salary'], dtype=np.float32)
+
+x_data = x_data.reshape((-1, 1))
+y_data = y_data.reshape((-1, 1))
+
+print(x_data.shape)
+print(y_data.shape)
 
 x_train, x_val, y_train, y_val = train_test_split(x_data, y_data, test_size=0.2, random_state=2021)
 
-print(x_train.shape, x_val.shape) 
-print(y_train.shape, y_val.shape) 
+print(x_train.shape, x_val.shape)
+print(y_train.shape, y_val.shape)
 
-model = Sequential([ Dense(1) ]) 
-model.compile(loss='mean_squared_error', optimizer=SGD(lr=0.01)) 
-model.fit( x_train, y_train, validation_data=(x_val, y_val), # 검증 데이터를 넣어주면 한 epoch이 끝날때마다 자동으로 검증 epochs=100 # epochs 복수형으로 쓰기! )
+model = Sequential([
+  Dense(1)
+])
 
-y_pred = model.predict(x_val) 
-          
-plt.scatter(x_val, y_val) 
-plt.scatter(x_val, y_pred, color='r') 
+model.compile(loss='mean_squared_error', optimizer=SGD(lr=0.01))
+
+model.fit(
+    x_train,
+    y_train,
+    validation_data=(x_val, y_val), # 검증 데이터를 넣어주면 한 epoch이 끝날때마다 자동으로 검증
+    epochs=100 # epochs 복수형으로 쓰기!
+)
+
+
+
+y_pred = model.predict(x_val)
+
+plt.scatter(x_val, y_val)
+plt.scatter(x_val, y_pred, color='r')
 plt.show()
